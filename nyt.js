8a0cdbda83155241ff574fd76a2029a335817b47
czarlos <cer26@duke.edu>
2014-08-14 00:06:25 -0400

    added outline for campaign finance

--- nyt.js
+++ nyt.js
@@ -47,7 +47,7 @@ function nyt (keys) {
 		'search':'lists.json', 
 		'history':'lists/best-sellers/history.json', 
 		'overview':'lists/overview.json', 
-		'names':'lists/name.json'}
+		'names':'lists/names.json'}
         
 		if (!callback) {
             callback = args;
@@ -57,6 +57,58 @@ function nyt (keys) {
         var path = '/svc/books/' + version + sub_api[which] + '?' + querystring.stringify(args) + '&' + 'api-key=' + keys.best_sellers;
         this.get(path, callback, args);
     }
+	
+	this.campaign_finance = function (args, which, callback) {
+		var sub_api = {'candidates': {
+							'search':'',
+							'details':'',
+							'leaders':'',
+							'state':'',
+							'new':''},
+						'committees': {
+							'search':'',
+							'details':'',
+							'new':'',
+							'contributions':'',
+							'contributions_to_candidate':'',
+							'filings':'',
+							'leadership':'' },
+						'presidential_campaigns': {
+							'candidate_totals':'',
+							'details':'',
+							'state_totals':'' },
+						'electronic': {
+							'candidate_totals':'',
+							'filing_form_types:'',
+							'filings_by_type':'',
+							'filing_summary':'',
+							'recent_amendments':''},
+						'electioneering_communications' : {
+							'recent':'',
+							'comittee':'',
+							'date':'' },
+						'individual_contributions': {
+							'by_presidential_filing':'',
+							'by_committee':'',
+							'by_candidate':''},
+						'independent_expenditures': {
+							'research':'',
+							'by_date':'',
+							'committee_independent':'',
+							'support_or_oppose':'',
+							'presidential':'',
+							'independent':'',
+							'race_totals':'',
+							'race_total_candidate':''},
+						'late_contributions': {
+							'today':'',
+							'to_candidate':'',
+							'independent':'',
+							'by_date':'' },
+						'lobbyist_bundlers': {
+							'to_a_committee':''}
+						}
+	}
 }
 
 module.exports = nyt;
