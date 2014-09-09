6a15b1db7ff7dc9bf75673313fbad8c88fe4bbcf
czarlos <cer26@duke.edu>
2014-09-09 02:50:03 -0400

    finished community api

--- community.js
+++ community.js
@@ -37,7 +37,6 @@ var byDate = function (args, callback, myKeys, t) {
     var path = (t.S).concat(t.SVC, t.S, 'community', t.S, t.V2, t.S, 'comments', t.S,
     						'by-date', t.S, date, t.D, format, t.Q, query, t.API_KEY, t.E,
     						myKeys['community']);
-    console.log(path);
     reql.get(path, callback, args);
 };
 
@@ -47,8 +46,15 @@ var byUser = function (args, callback, myKeys, t) {
     callback = callbackReturn.callback;
 
     var format = response_lib.checkField(args['format'], t.DEFAULT);
+    var user = response_lib.checkField(args['user-ID'], '');
+    response_lib.removeArgs(['user-ID'], args);
     var query = response_lib.checkQuery(args);
 
+    var path = (t.S).concat(t.SVC, t.S, 'community', t.S, t.V2, t.S, 'comments', t.S,'user',
+    						t.S, 'id', t.S, user, t.D, format, t.Q, query, t.API_KEY, t.E, 
+    						myKeys['community']);
+    
+    reql.get(path, callback, args);
 };
 
 var byURL = function (args, callback, myKeys, t) {
@@ -57,8 +63,15 @@ var byURL = function (args, callback, myKeys, t) {
     callback = callbackReturn.callback;
 
     var format = response_lib.checkField(args['format'], t.DEFAULT);
+	var match = response_lib.checkField(args['match-type'], '');
+    response_lib.removeArgs(['match-type'], args);
     var query = response_lib.checkQuery(args);
 
+    var path = (t.S).concat(t.SVC, t.S, 'community', t.S, t.V2, t.S, 'comments', t.S,'user',
+    						t.S, 'id', t.S, match, t.D, format, t.Q, query, t.API_KEY, t.E, 
+    						myKeys['community']);
+    
+    reql.get(path, callback, args);
 }
 
 exports.recent = recent;
